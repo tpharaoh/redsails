46fb1685106a8194cebf5cd5cac75af7e5eaa69e
Tim S <tim@live.fi>
2018-12-09 00:52:01 +0200

    initial working version

--- api/hooks/nodered/index.js
+++ api/hooks/nodered/index.js
@@ -1,25 +1,52 @@
 module.exports = (sails) => {
 
     return {
-      RED: {},
+        configure: function() {
+
+            // If SSL is on, use the HTTPS endpoint
+           sails.log.debug("pre red"+sails)
+         },
       initialize: (cb) => {
-              sails.on('lifted', async () => {
-                  RED = require("node-red");
-                  // Create the settings object - see default settings.js file for other options
-                  var settings = {
-                          httpAdminRoot:"/red",
-                          httpNodeRoot: "/api",
-                          userDir:"/home/tim/.node-red/", // change it to point to your user home dir
-                          functionGlobalContext: { }    // enables global context
-                  };
-  
-                  // Initialise the runtime with a server and settings
-                  RED.init(sails,settings);
-  
-                  // Start the runtime
-                  RED.start();
-              });
-              return cb();
-            }
+            let RED = require("node-red");
+            // Create the settings object - see default settings.js file for other options
+            let settings = {
+                    httpAdminRoot:"/red",
+                    httpNodeRoot: "/api",
+                    userDir:"/home/tim/.nodered/", // change it to point to your user home dir
+                    functionGlobalContext: { },    // enables global context
+                    httpNodeMiddleware: function (req, res, next) {
+                      
+                        const env = process.env.NODE_ENV || 'development';
+                        sails.log.debug('<<------------------------------');
+                        sails.log.debug("Requested data :: ");
+                        sails.log.debug('  ', req.method, req.url);
+                        if (env.toLowerCase() !== 'production') {
+                            sails.log.debug('   Headers:');
+                            sails.log.debug(req.headers);
+                            sails.log.debug('   Params:');
+                            sails.log.debug(req.params);
+                            sails.log.debug('   Body:');
+                            sails.log.debug(req.body);
+                        }
+                        sails.log.debug('------------------------------>>');
+                        return next();
+                    },
+            };
+
+            // Initialise the runtime with a server and settings
+            RED.init(sails.hooks.http.server,settings);
+
+            // Serve the editor UI from /red
+            sails.hooks.http.app.use(settings.httpAdminRoot, RED.httpAdmin);
+
+            // Serve the http nodes UI from /api
+            sails.hooks.http.app.use(settings.httpNodeRoot, RED.httpNode);
+
+            // Start the runtime
+            RED.start();
+        return cb();
+        
+        }
+          //sails.log.debug(sails.loggedInUser)
       }
   }
--- config/routes.js
+++ config/routes.js
@@ -33,8 +33,6 @@ module.exports.routes = {
   'GET /account/password':   { action: 'account/view-edit-password' },
   'GET /account/profile':    { action: 'account/view-edit-profile' },
 
-  'GET /red': 'NodeRedController.admin',
-  'GET /api': 'NodeRedController.api',
 
   //  ╔╦╗╦╔═╗╔═╗  ╦═╗╔═╗╔╦╗╦╦═╗╔═╗╔═╗╔╦╗╔═╗   ┬   ╔╦╗╔═╗╦ ╦╔╗╔╦  ╔═╗╔═╗╔╦╗╔═╗
   //  ║║║║╚═╗║    ╠╦╝║╣  ║║║╠╦╝║╣ ║   ║ ╚═╗  ┌┼─   ║║║ ║║║║║║║║  ║ ║╠═╣ ║║╚═╗
